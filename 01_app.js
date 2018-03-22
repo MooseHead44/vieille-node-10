8622247b86a67667949133bb4b7b5a0a32878a28
William Pelletier <william_pelletier@hotmail.ca>
2018-03-22 14:25:38 -0400

    ajout de l'exercice 9

--- 01_app.js
+++ 01_app.js
@@ -5,11 +5,7 @@ const app = express();
 const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient; // le pilote MongoDB
 const ObjectID = require('mongodb').ObjectID;
-const http = require('http');
-const server = http.createServer(app);
-const io = require('./mes_modules/chat_socket').listen(server);
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 /* on associe le moteur de vue au module «ejs» */
 app.use(express.static('public'));
 const i18n = require('i18n');
@@ -142,4 +138,24 @@ app.get('/vider', (req, res) => {
 	res.redirect('/adresse')
 })
 
+app.post('/ajax_modifier', (req,res) => {
+    req.body._id =  ObjectID(req.body._id)
+    db.collection('adresse').save(req.body, (err, result) => {
+         if (err) return console.log(err)
+         res.send(JSON.stringify(req.body))
+   })
+})
 
+app.post('/ajax_ajouter', (req,res) => {  
+ db.collection('adresse').save(req.body, (err, result) => {
+ if (err) return console.log(err)
+    res.send(JSON.stringify(req.body))
+ })
+})
+
+app.post('/ajax_detruire/', (req, res) => {
+ db.collection('adresse').findOneAndDelete({"_id": ObjectID(req.body._id)}, (err, resultat) => {
+if (err) return console.log(err)
+ res.send(JSON.stringify(resultat))  // redirige vers la route qui affiche la collection
+ })
+})
